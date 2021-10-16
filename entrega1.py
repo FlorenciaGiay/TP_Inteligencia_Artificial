b524b186f11dc935a1aaa2fdf1536903d0323cb8
FlorenciaGiay <floo.giay@gmail.com>
2021-10-16 15:06:53 -0300

    Funciones result, is goal y heuristica.

--- entrega1.py
+++ entrega1.py
@@ -4,21 +4,120 @@ from simpleai.search import (
     astar,
 )
 
-# Metodo para convertir tupla a lista
-def convertir_a_lista(tupla):
-    lista = list(tupla)
-    return lista
+# Método para convertir tuplas a listas  
+def convertir_a_listas(tuplas):
+    return [list(x) for x in tuplas]
 
-# Metodo para convertir lista a tupla
-def convertir_a_tupla(lista):
-    tupla = tuple(lista)
-    return tupla
+# Método para convertir listas a tuplas
+def convertir_a_tuplas(listas):
+    return tuple([tuple(x) for x in listas])
+
+# Clase problem
+class ExploracionRobotica(SearchProblem):    
+    tuneles = ( (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6) )
+
+    # El costo es el tiempo en minutos
+    def cost(self, state1, action, state2):
+        robot, tipo_accion, destino_accion = action
+        if(tipo_accion == 'mover'):
+            return 1
+        else:
+            return 5
     
+    def actions(self, state):
+        # Las acciones son tuplas de 3 elementos: (robot, tipo_accion, destino_accion)
+        # tipo_accion: mover o cargar
+        # destino accion: posición a moverse (1,1) o el robot a cargar (e1)
+        acciones_disponibles = []
+
+        for elemento in state:
+            accion = []
+
+            # Si el largo de elemento es 3 siginfica que es escaneador (el tercer dato es la batería)
+            if len(elemento) == 3:
+                robot, posicion, bateria = elemento
+                if bateria >= 100:
+                    posiciones_posibles = []
+                    posiciones_posibles.append(posicion[0]-1, posicion[1])
+                    posiciones_posibles.append(posicion[0]+1, posicion[1])
+                    posiciones_posibles.append(posicion[0], posicion[1]-1)
+                    posiciones_posibles.append(posicion[0], posicion[1]+1)
+
+                    # Generar acciones de tipo mover a las posiciones que sean túneles
+                    for posicion_posible in posiciones_posibles:
+                        if posicion_posible in self.tuneles:
+                            acciones_disponibles.append((robot, 'mover', posicion_posible))
+
+            # Si el largo de elemento no es 3 siginfica que es soporte (no tiene el dato de 
+            # la batería ya que es infinita)
+            else:
+                robot, posicion = elemento 
+                posiciones_posibles = []
+                posiciones_posibles.append(posicion[0]-1, posicion[1])
+                posiciones_posibles.append(posicion[0]+1, posicion[1])
+                posiciones_posibles.append(posicion[0], posicion[1]-1)
+                posiciones_posibles.append(posicion[0], posicion[1]+1)
+
+                # Generar acciones de tipo mover a las posiciones que sean túneles
+                for posicion_posible in posiciones_posibles:
+                    if posicion_posible in self.tuneles:
+                        acciones_disponibles.append((robot, 'mover', posicion_posible))
+                
+                # Generar acciones de tipo carga con los robots escaneadores que necesitan carga y 
+                # están en la misma posición que el robot de soporte 
+                for objeto in state:
+                    if len(objeto) == 3 and objeto[1] == posicion and objeto[2] < 1000:
+                        acciones_disponibles.append((robot, 'cargar', objeto[0]))
+
+        return acciones_disponibles
+
+    # Lista de los túneles que van recorriendo los robots
+    tuneles_recorridos = []
+
+    def result(self, state, action):
+        estado_lista = convertir_a_listas(state)
+        robot, tipo_accion, destino_accion = action
+
+        if(tipo_accion == 'mover'):
+            # Modificar el estado asignándole al robot correspondiente la nueva posición
+            # y si es escaneador, le resto 100 a la batería
+            for elemento in estado_lista:
+                if elemento[0] == robot:
+                    elemento[1] = destino_accion
+                    if len(elemento) == 3:
+                        elemento[2] = elemento[2] - 100
+                        
+                        # Agregar la coordenada del tunel que se está escaneando a 
+                        # la lista de tuneles recorridos
+                        if destino_accion not in self.tuneles_recorridos:
+                            self.tuneles_recorridos.append(destino_accion)
+        else:
+                # Si el tipo de acción es cargar, modifico en el estado la batería del robot
+                # al que se quiere cargar a 1000
+                for elemento in estado_lista:
+                    if elemento[0] == destino_accion:
+                        elemento[2] = 1000        
+        
+        estado_tupla = convertir_a_tuplas(estado_lista)
+
+        return  estado_tupla
+    
+    def is_goal(self, state):
+        # Retornar true si el largo de túneles recorridos y túneles es igual,
+        # lo que significa que se escanearon todas las posiciones del túnel
+        condicion = len(self.tuneles_recorridos) == len(self.tuneles)
+        return condicion
+    
+    def heuristic(self, state):
+        # La heurística es la cantidad de túneles que faltan recorrer multiplicados por 1 minuto
+        tiempo = [len(self.tuneles) - len(self.tuneles_recorridos)] * 1
+        return tiempo
+
 
 def planear_escaneo(tuneles, robots):
 
-    # El estado es una tupla de tuplas con el tipo de robot, la posicion que tambien es tupla 
-    # y si es mapeador la bateria actual
+    # El estado es una tupla de tuplas con el tipo de robot, la posición que también es tupla 
+    # y si es mapeador la batería actual
     estado_inicial = []
     for robot in robots:
         if(robot[1] == 'escaneador'):
@@ -26,87 +125,28 @@ def planear_escaneo(tuneles, robots):
         else:
             tupla = (robot[0], (5,0))
         estado_inicial.append(tupla)
-    estado_tupla = convertir_a_tupla(estado_inicial)
-
-    # Clase problem
-    class ExploracionRobotica(SearchProblem):    
-
-        # Lista de los tuneles que van recorriendo los robots
-        tuneles_recorridos = []
-
-        def cost(self, state1, action, state2):
-            robot, tipo_accion, destino_accion = action
-            if(tipo_accion == 'mover'):
-                return 1
-            else:
-                return 5
         
-        def actions(self, state):
-            # Las acciones seran tuplas de 3 elementos: (robot, tipo accion, destino accion)
-            # tipo accion: mover o cargar
-            # destino accion: posicion a moverse (1,1) o el robot a cargar
-            acciones_disponibles = []
-
-            for elemento in state:
-                accion = []
-
-                # Si el largo de elemento es 3 siginfica que es escaneador (el tercer dato es la bateria)
-                if len(elemento) == 3:
-                    robot, posicion, bateria = elemento
-                    if bateria >= 100:
-                        posiciones_posibles = []
-                        posiciones_posibles.append(posicion[0]-1, posicion[1])
-                        posiciones_posibles.append(posicion[0]+1, posicion[1])
-                        posiciones_posibles.append(posicion[0], posicion[1]-1)
-                        posiciones_posibles.append(posicion[0], posicion[1]+1)
-
-                        # Generar acciones de tipo mover a las posiciones que sean tuneles
-                        for posicion_posible in posiciones_posibles:
-                            if posicion_posible in tuneles:
-                                acciones_disponibles.append((robot, 'mover', posicion_posible))
-
-                # Si el largo de elemento no es 3 siginfica que es soporte (no tiene el dato de la bateria ya que es infinita)
-                else:
-                    robot, posicion = elemento 
-                    posiciones_posibles = []
-                    posiciones_posibles.append(posicion[0]-1, posicion[1])
-                    posiciones_posibles.append(posicion[0]+1, posicion[1])
-                    posiciones_posibles.append(posicion[0], posicion[1]-1)
-                    posiciones_posibles.append(posicion[0], posicion[1]+1)
-
-                    # Generar acciones de tipo mover a las posiciones que sean tuneles
-                    for posicion_posible in posiciones_posibles:
-                        if posicion_posible in tuneles:
-                            acciones_disponibles.append((robot, 'mover', posicion_posible))
-                    
-                    # Generar acciones de tipo carga con los robots escaneadores que necesitan carga y 
-                    # estan en la misma posicion que el robot de soporte 
-                    for objeto in state:
-                        if len(objeto) == 3 and objeto[1] == posicion and objeto[2] < 1000:
-                            acciones_disponibles.append((robot, 'cargar', objeto[0]))
-
-            return acciones_disponibles
-                        
-        def result(self, state, action):
-            return state
+    estado_tupla = tuple(estado_inicial)
         
-        def is_goal(self, state):
-            # comparar tuneles_recorridos con tuneles elemento por elemento
-            # o el largo de las listas
-            return 
-        
-        def heuristic(self, state):
-            return 0
-        
-    problem = ExploracionRobotica(estado_inicial)
+    problem = ExploracionRobotica(estado_tupla)
 
     result = astar(problem, graph_search=True)
 
-    print("Goal node:", result)
+    plan = []
 
-    print("Path from initial to goal:")
-    for action, state in result.path():
-        print("Action:", action)
-        print("State:", state)
-    
-    return result
+    for action in result.path():
+        plan.append(action)
+
+    plan_tupla = tuple(plan)
+    return plan_tupla
+
+
+# Prueba 
+
+tuneles = ( (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6) )
+
+robots = ( ("e1", "escaneador"), 
+            ("e2", "escaneador"), 
+            ("e3", "escaneador"), 
+            ("s1", "soporte"), 
+            ("s2", "soporte"))
